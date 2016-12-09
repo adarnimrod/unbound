a56ae486d991214d868c91973e6c9eb0175495b6
Adar Nimrod <nimrod@shore.co.il>
2016-12-09 19:40:18 +0200

    - Use SystemInfo to determine to OS type (instead of using the facts from the Ansbile module and the Testinfra backend module, which is just ugly code).

--- tests/test_unbound.py
+++ tests/test_unbound.py
@@ -16,31 +16,31 @@ def test_unbound_socket(Socket):
     assert Socket('udp://127.0.0.1:53').is_listening
 
 
-def test_unbound_conf_d(File, Ansible):
-    ansible_os_family = Ansible('setup')['ansible_facts']['ansible_os_family']
-    if ansible_os_family == 'OpenBSD':
+def test_unbound_conf_d(File, SystemInfo):
+    if SystemInfo.type == 'openbsd':
         assert 'include: "/var/unbound/etc/unbound.conf.d/*.conf"' in File(
                 '/var/unbound/etc/unbound.conf').content_string
         assert File('/var/unbound/etc/unbound.conf.d').is_directory
-    elif ansible_os_family == 'Debian':
+    elif SystemInfo.type == 'linux' and SystemInfo.distribution in ['debian',
+                                                                    'ubuntu']:
         assert 'include: "/etc/unbound/unbound.conf.d/*.conf"' in File(
                 '/etc/unbound/unbound.conf').content_string
         assert File('/etc/unbound/unbound.conf.d').is_directory
 
 
-def test_unbound_trust_anchor(Ansible, File):
-    ansible_os_family = Ansible('setup')['ansible_facts']['ansible_os_family']
-    if ansible_os_family == 'OpenBSD':
+def test_unbound_trust_anchor(SystemInfo, File):
+    if SystemInfo.type == 'openbsd':
         assert File('/var/unbound/etc/root.key').exists
-    elif ansible_os_family == 'Debian':
+    elif SystemInfo.type == 'linux' and SystemInfo.distribution in ['debian',
+                                                                    'ubuntu']:
         assert File('/etc/unbound/root.key').exists
 
 
-def test_unbound_user(Ansible, User, File):
-    ansible_os_family = Ansible('setup')['ansible_facts']['ansible_os_family']
-    if ansible_os_family == 'OpenBSD':
+def test_unbound_user(SystemInfo, User, File):
+    if SystemInfo.type == 'openbsd':
         assert User('_unbound').exists
         assert File('/etc/mail/aliases').contains('_unbound: root')
-    elif ansible_os_family == 'Debian':
+    elif SystemInfo.type == 'linux' and SystemInfo.distribution in ['debian',
+                                                                    'ubuntu']:
         assert User('unbound').exists
         assert File('/etc/aliases').contains('unbound: root')
